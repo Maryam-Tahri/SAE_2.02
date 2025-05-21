7f566da649058b5044ea33057d0dbf7408d930a1
WISPELAERE Mathis <e66018u@etu.univ-lorraine.fr>
2025-05-21 10:52:28 +0200

    Avancement GrapheListe

--- Projet_IntelliJ/src/GrapheListe.java
+++ Projet_IntelliJ/src/GrapheListe.java
@@ -1,3 +1,19 @@
-public class GrapheListe implements Graphe{
+import java.util.ArrayList;
 
+public class GrapheListe implements Graphe{
+    private ArrayList<String> Noeuds;
+    private ArrayList<Arcs> adjacence;
+
+    public void ajouterArc(String depart, String destination, double count){
+        throw new Error("TODO");
+    }
+
+    public int getIndice(String n){
+        for (int i = 0; i < Noeuds.size(); i++) {
+            if (Noeuds.get(i).equals(n)){
+                return i;
+            }
+        }
+        return -1;
+    }
 }
