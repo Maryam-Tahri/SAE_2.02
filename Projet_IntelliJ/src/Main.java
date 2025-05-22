966509c3c8a080d79f5921fb6421753e69034271
MathisWISPELAERE <e66018u@etu.univ-lorraine.fr>
2025-05-22 20:47:23 +0200

    modif main comparaison BellmanFord Dijkstra

--- Projet_IntelliJ/src/Main.java
+++ Projet_IntelliJ/src/Main.java
@@ -17,14 +17,42 @@ public class Main {
         Valeurs v =  new Valeurs();
         BellmanFord bf = new BellmanFord();
         v = bf.resoudre(graph,"A");
-        System.out.println(v.toString());
+        //System.out.println(v.toString());
 
         Valeurs v2 =  new Valeurs();
         Dijkstra dj = new Dijkstra();
         v = dj.resoudre(graph,"A");
-        System.out.println(v.toString());
+        //System.out.println(v.toString());
 
         GrapheListe g = new GrapheListe("Graphe/Graphe1.txt");
-        System.out.println(g.toString());
+        //System.out.println(g.toString());
+        v = dj.resoudre(g,"1");
+        v2 = bf.resoudre(g,"1");
+        System.out.println("Graphe 1 Dijkstra :"+v.toString() + "\n\nGraphe 1 BellmanFord :" + v2.toString());
+
+
+        g = new GrapheListe("Graphe/Graphe15.txt");
+        v = dj.resoudre(g,"1");
+        v2 = bf.resoudre(g,"1");
+        System.out.println("Graphe 15 Dijkstra :"+v.toString() + "\n\nGraphe 15 BellmanFord :" + v2.toString());
+
+
+        g = new GrapheListe("Graphe/Graphe31.txt");
+        v = dj.resoudre(g,"1");
+        v2 = bf.resoudre(g,"1");
+        System.out.println("Graphe 31 Dijkstra :"+v.toString() + "\n\nGraphe 31 BellmanFord :" + v2.toString());
+
+        g = new GrapheListe("Graphe/Graphe45.txt");
+        v = dj.resoudre(g,"1");
+        v2 = bf.resoudre(g,"1");
+        System.out.println("Graphe 45 Dijkstra :"+v.toString() + "\n\nGraphe 45 BellmanFord :" + v2.toString());
+
+
+        g = new GrapheListe("Graphe/Graphe66.txt");
+        v = dj.resoudre(g,"1");
+        v2 = bf.resoudre(g,"1");
+        System.out.println("Graphe 66 Dijkstra :"+v.toString() + "\n\nGraphe 66 BellmanFord :" + v2.toString());
+
+
     }
 }
