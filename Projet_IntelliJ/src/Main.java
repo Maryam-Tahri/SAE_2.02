ebce4431238bbf31f7f6a5fc035ff06d3ed54a6e
MathisWISPELAERE <e66018u@etu.univ-lorraine.fr>
2025-05-23 10:44:53 +0200

    ajout comparaison BellmanFord - Dijkstra

--- Projet_IntelliJ/src/Main.java
+++ Projet_IntelliJ/src/Main.java
@@ -26,33 +26,60 @@ public class Main {
 
         GrapheListe g = new GrapheListe("Graphe/Graphe1.txt");
         //System.out.println(g.toString());
+        long datedeb1 = System.nanoTime();
         v = dj.resoudre(g,"1");
+        long datefin1 = System.nanoTime();
+        long dateexec1 = datefin1-datedeb1;
+        long datedeb2 = System.nanoTime();
         v2 = bf.resoudre(g,"1");
-        System.out.println("Graphe 1 Dijkstra :"+v.toString() + "\n\nGraphe 1 BellmanFord :" + v2.toString());
+        long datefin2 = System.nanoTime();
+        long dateexec2 = datefin2-datedeb2;
+        System.out.println("Graphe 1 Dijkstra :\n"+v.toString() + "\ntemps d'éxecution de Dijkstra ; " + dateexec1 +"ms\nGraphe 1 BellmanFord :\n" + v2.toString() +"\nTemps d'éxecution de bellmanFord\n"+dateexec2+"ms");
 
 
         g = new GrapheListe("Graphe/Graphe15.txt");
+        datedeb1 = System.nanoTime();
         v = dj.resoudre(g,"1");
+        datefin1 = System.nanoTime();
+        dateexec1 = datefin1-datedeb1;
+        datedeb2 = System.nanoTime();
         v2 = bf.resoudre(g,"1");
-        System.out.println("Graphe 15 Dijkstra :"+v.toString() + "\n\nGraphe 15 BellmanFord :" + v2.toString());
-
+        datefin2 = System.nanoTime();
+        dateexec2 = datefin2-datedeb2;
+        System.out.println("Graphe 15 Dijkstra :\n"+v.toString() + "\ntemps d'éxecution de Dijkstra :" + dateexec1 +"ms\nGraphe 15 BellmanFord :\n" + v2.toString() +"\nTemps d'éxecution de bellmanFord\n"+dateexec2+"ms");
 
         g = new GrapheListe("Graphe/Graphe31.txt");
+        datedeb1 = System.nanoTime();
         v = dj.resoudre(g,"1");
+        datefin1 = System.nanoTime();
+        dateexec1 = datefin1-datedeb1;
+        datedeb2 = System.nanoTime();
         v2 = bf.resoudre(g,"1");
-        System.out.println("Graphe 31 Dijkstra :"+v.toString() + "\n\nGraphe 31 BellmanFord :" + v2.toString());
+        datefin2 = System.nanoTime();
+        dateexec2 = datefin2-datedeb2;
+        System.out.println("Graphe 31 Dijkstra :\n"+v.toString() + "\ntemps d'éxecution de Dijkstra ; " + dateexec1 +"ms\nGraphe 31 BellmanFord :\n" + v2.toString() +"\nTemps d'éxecution de bellmanFord : "+dateexec2+"ms");
 
         g = new GrapheListe("Graphe/Graphe45.txt");
+        datedeb1 = System.nanoTime();
         v = dj.resoudre(g,"1");
+        datefin1 = System.nanoTime();
+        dateexec1 = datefin1-datedeb1;
+        datedeb2 = System.nanoTime();
         v2 = bf.resoudre(g,"1");
-        System.out.println("Graphe 45 Dijkstra :"+v.toString() + "\n\nGraphe 45 BellmanFord :" + v2.toString());
-
+        datefin2 = System.nanoTime();
+        dateexec2 = datefin2-datedeb2;
+        System.out.println("Graphe 45 Dijkstra :\n"+v.toString() + "\ntemps d'éxecution de Dijkstra : " + dateexec1 +"ms\nGraphe 45 BellmanFord :\n" + v2.toString() +"\nTemps d'éxecution de bellmanFord : "+dateexec2+"ms");
 
         g = new GrapheListe("Graphe/Graphe66.txt");
+        datedeb1 = System.nanoTime();
         v = dj.resoudre(g,"1");
+        datefin1 = System.nanoTime();
+        dateexec1 = datefin1-datedeb1;
+        datedeb2 = System.nanoTime();
         v2 = bf.resoudre(g,"1");
-        System.out.println("Graphe 66 Dijkstra :"+v.toString() + "\n\nGraphe 66 BellmanFord :" + v2.toString());
-
+        datefin2 = System.nanoTime();
+        dateexec2 = datefin2-datedeb2;
+        System.out.println("Graphe 66 Dijkstra :\n"+v.toString() + "\ntemps d'éxecution de Dijkstra : " + dateexec1 +"ms\nGraphe 66 BellmanFord :\n" + v2.toString() +"\nTemps d'éxecution de bellmanFord : "+dateexec2+"ms");
 
     }
 }
